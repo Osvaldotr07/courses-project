df76a5040cdbec44fa1985eacc3b9dfc6551643d
osvaldo <osvaldotr07@gmail.com>
2020-08-19 16:50:18 -0500

    master

--- assets/js/form.js
+++ assets/js/form.js
@@ -23,6 +23,7 @@ submitButton.addEventListener("click", async (evt) => {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "*",
         },
       }
     );
